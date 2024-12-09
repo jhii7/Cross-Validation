12283eb05999c5837274e729b9f0572c056f4419
LiMoricz <Lianamoricz@gmail.com>
2024-12-09 01:52:58 +0000

    F1 file completed and Final.py file created to run both files.

--- F1compare.py
+++ F1compare.py
@@ -1,60 +1,61 @@
-
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import f1_score, make_scorer
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.metrics import f1_score
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
 
+# Here, we compute the F1 scores for both training/testing sets for Train Test split and testing sets across all k-folds in Cross Validation.
+# Higher F1 score indicates a higher recall and precision, resulting in better and more accurate classification.
+#
 
- ##### NOT FINISHED wil not run
-## TO RUN FILE ON dataset1:
-
-
-#       python F1compare.py
-
-#       TestingSets/dataset1.csv
-
-
-file_path = input("Enter the path to the CSV file: ")
-data = pd.read_csv(file_path, header=None)
-
-print(data.head()) 
-# Print column names
-print(data.columns)  
-
-X = data[0].values.reshape(-1, 1)  # First col = feature
+filepath = input("Enter path to the CSV: ")
+data = pd.read_csv(filepath, header=None)
+X = data[0].values.reshape(-1, 1) 
 y = data[1].values
 
+# Adjusted threshold
+thresh = np.median(y)
+yclass = (y > thresh).astype(int)
 
-## Single Train Test Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-model = LogisticRegression()
-
-model.fit(X_train, y_train)
-y_pred_train = model.predict(X_train)
-y_pred_test = model.predict(X_test)
-
-print("Train-Test Split, Logistic Regression F1 Score Training Data:")
-training_f1 = f1_score(y_train, y_pred_train, average='weighted')
-print(training_f1)
-print("Train-Test Split, Logistic Regression F1 Score Testing Data:")
-testing_f1 = f1_score(y_test, y_pred_test, average='weighted')
-print(testing_f1)
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+smote = SMOTE(random_state=42)
+Xresample, yresample = smote.fit_resample(X, yclass)
 
 
 
-## K FOLD
+# Train Test Split F1
+X_train, X_test, y_train, y_test = train_test_split(Xresample, yresample, test_size=0.2, random_state=0)
+model = LogisticRegression().fit(X_train, y_train)
+
+ytrainpred = model.predict(X_train)
+ytestpred = model.predict(X_test)
+trainf1 = f1_score(y_train, ytrainpred)
+testf1 = f1_score(y_test, ytestpred)
+
+print("Train-Test Split, Logistic Regression (F1):")
+print(f"F1 Score (Training Data): {trainf1:.2f}")
+print(f"F1 Score (Testing Data): {testf1:.2f}")
 
 
+
+# K-Fold Cross Validation F1 (5)
 k = 5
 kf = KFold(n_splits=k, shuffle=True, random_state=42)
-f1_scoreE = make_scorer(f1_score, average='weighted')
-scores = cross_val_score(model, X, y, cv=kf, scoring=f1_scoreE)
+model = LogisticRegression()
 
+f1_scores = []
 
-## Results
-average_f1 = np.mean(scores)
-print("K-folds, Logistic Regression F1 Scores: ")
-print(f"F1 Score for each fold: {[round(score, 4) for score in scores]}")
-print(f"Average F1 Score across {k} folds: {average_f1:.4f}")
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = yclass[train_index], yclass[test_index]
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    f1_scores.append(float(f1_score(y_test, y_pred)))  # Convert to Python float
+
+average_f1 = np.mean(f1_scores)
+print("\nK-folds, Logistic Regression (F1, Representing Testing Sets):")
+print(f"F1 Score for each fold: {[round(score, 4) for score in f1_scores]}")
+print(f"Average F1 Score across {k} folds: {average_f1:.2f}")
--- R2compare.py
+++ R2compare.py
@@ -5,43 +5,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 
-
-## TO RUN FILE ON dataset1:
-
-
-#       python R2compare.py
-
-#       TestingSets/dataset1.csv
-
-# !!!
 # Here, we get the R^2 (coefficient of determination) values for training and testing data.
 # "R-squared is a statistical measure that indicates how much of the variation of a dependent variable is explained by an independent variable in a regression model."
 # In essence, the higher the score, the better predictive performance. 
-# !!!
 
-# Train-Test-Split #
-
-# Can someone get this part working? Use X and y below in the meantime as a test set. 
-# data1 = pd.read_csv("dataset1.csv", sep='/t', header=None, engine='python')
-# X = data1.iloc[:, 0]
-# y = data1.iloc[:, -1]
-
-###X = np.arange(20).reshape(-1, 1)
-###y = np.array([5, 12, 11, 19, 30, 29, 23, 40, 51, 54, 74, 62, 68, 73, 89, 84, 89, 101, 99, 106])
-
-
-file_path = input("Enter the path to the CSV file: ")
-data = pd.read_csv(file_path, header=None)
-
-print(data.head()) 
-# Print column names
-print(data.columns)  
-
-
+filepath = input("Enter the path to the CSV: ")
+data = pd.read_csv(filepath, header=None)
 X = data[0].values.reshape(-1, 1)  # First col =  feature
 y = data[1].values  # Second col =  target
+print(" ")
 
 
+# Train-Test-Split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 model = LinearRegression().fit(X_train, y_train)
@@ -54,12 +29,7 @@ print(testingScore)
 
 
 
-
-# K-Folds (5) #
-
-###X = np.arange(20).reshape(-1, 1)
-###y = np.array([5, 12, 11, 19, 30, 29, 23, 40, 51, 54, 74, 62, 68, 73, 89, 84, 89, 101, 99, 106])
-
+# K-Folds (5) 
 k = 5
 kf = KFold(n_splits=k, shuffle=True, random_state=42)
 model = LinearRegression()
